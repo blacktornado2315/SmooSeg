241a146ef4a5ce61faa9c7f481eb7a785d82434a
blacktornado2315 <blacktornado2315@gmail.com>
2024-11-19 21:04:18 +0530

    Update train_segmentation.py
    added polynomial decay

--- train_segmentation.py
+++ train_segmentation.py
@@ -109,7 +109,9 @@ class LitUnsupervisedSegmenter(pl.LightningModule):
             {'params': list(self.projection.parameters()), 'lr': self.cfg.lr1, 'weight_decay': 1e-5},
             {'params': list(self.prediction.parameters()), 'lr': self.cfg.lr2, 'weight_decay': 1e-5}
         ])
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+        def poly_lr_scheduler(step, max_steps, power=1.0):
+            return (1-step/max_steps)**power
+        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda step: poly_lr_scheduler(step, self.cfg.max_steps))
         return [optimizer], [scheduler]
 
     def on_train_start(self):
